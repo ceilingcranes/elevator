10479a65c3e9ab6d8a65b2e68482c228c6b5befd
Maxine Hartnett <maxine.hartnett@nordstrom.com>
2022-01-17 12:23:03 -0700

    Tweaking exceptions

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
+from math import floor
 import elevator
 
 def get_floor_list_input(max_floor):
     is_error = True
     while(is_error):
-        floor_list = input("Please enter a comma seperated list of requested floors (or nothing to end program):\n")
-        floor_list = [int(x) for x in floor_list.split(",")]
+        try:
+            floor_list = input("Please enter a comma seperated list of requested floors (or nothing to end program):\n")
+            # check to see if the input was an empty string
+            if(floor_list): 
+                floor_list = [int(x) for x in floor_list.split(",")]
 
-        if (sum([x>max_floor for x in floor_list]) or min(floor_list)<=0):
-            print("Invalid floor request. Please enter a comma seperated list of positive integers that are less than the max floor.")
-        else:
+                if (sum([x>max_floor for x in floor_list]) or min(floor_list)<=0):
+                    raise IndexError
+            
             is_error = False
+        except KeyboardInterrupt:
+            # Allow program to be ended with ctrl+c
+            return []
+        except:
+            print("Invalid floor request. Please enter a comma seperated list of positive integers that are less than the max floor.")
+
     return floor_list
 
 def main():
@@ -21,6 +31,7 @@ def main():
 
         if(start_floor < 0 or max_floor<0 or start_floor>max_floor):
             raise ValueError
+            
     except ValueError:
         print("Invalid value. Please enter one positive integer for start and max floors, and a comma-seperated list of integers for the floor list.")
     
@@ -38,10 +49,6 @@ def main():
 
         print("Total time: {} seconds".format(total_time))
         floor_list = get_floor_list_input(max_floor)
-
-        
-        
-
 
 
 if __name__=="__main__":
